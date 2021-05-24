7b45d2f6b77daab10d07faccb7c37986d6b69091
Adriana Diniz <dri.ragazoni@gmail.com>
2021-05-24 01:33:24 -0300

    first commit

---
+++ src/test/java/com/bootcamp/crudclients/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.crudclients.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
